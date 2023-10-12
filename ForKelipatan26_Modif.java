85d075a28279f8b1a06a8c6bb92437d0e2a8867e
Sariafauzani <ajas2654@gmail.com>
2023-10-12 10:14:20 +0700

    modif

--- ForKelipatan26_Modif.java
+++ ForKelipatan26_Modif.java
@@ -7,7 +7,7 @@ public class ForKelipatan26_Modif {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int kelipatan, jumlah = 0, counter = 0;
-        double rata-rata;
+        double rataRata;
         System.out.print ("Masukkan bilangan kelipatan (1-9): ");
         kelipatan = scan.nextInt();
 
@@ -17,9 +17,11 @@ public class ForKelipatan26_Modif {
                 counter++; 
             }
         }
-        
+        if (counter > 0) {
+           rataRata = (double) jumlah / counter;
          System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
          System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
-        
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, rataRata);
     }
+}
 }
